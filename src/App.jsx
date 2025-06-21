ad07411b30af7c7a657286b2e755f10ed010cbc9
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-21 23:07:35 +0630

    animated the hero section
    animated title, paragraph and leaf

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { SplitText, ScrollTrigger } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/navbar';
+import Hero from './components/Hero';
 gsap.registerPlugin(SplitText, ScrollTrigger);
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <Hero/>
+      <div className='h-dvh bg-black'/>
     </main>
   )
 }
