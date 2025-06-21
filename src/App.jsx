f57c4f8727a2035e99724a63bc1369425098b2b8
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-21 18:41:18 +0630

    navbar and public
    added navbar inbetween animation with gsap.timeline and added required public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SplitText, ScrollTrigger } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './components/navbar';
 gsap.registerPlugin(SplitText, ScrollTrigger);
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-5xl text-red-500'>HELLO REACT, TAILWIND, GSAP!</h1>
-      </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
