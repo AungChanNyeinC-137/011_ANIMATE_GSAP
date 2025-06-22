8f106548a7a4e94158b62f02e993e249591a0c35
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-06-22 13:04:34 +0630

    solved build fail in deployment
    case sensitivity issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SplitText, ScrollTrigger } from 'gsap/all'
 import gsap from 'gsap'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 gsap.registerPlugin(SplitText, ScrollTrigger);
